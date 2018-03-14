9dbcc9d8c04f4b43497345aa79b3b9f6e0a92ec2
vveliev <vveliev@tucows.com>
2018-03-14 02:09:14 -0400

    reverting tank api virtual env, as it makes no difference , need to reed more docs on how to make api work and what configs should be.

--- docker/yandex-tank-api/entrypoint.sh
+++ docker/yandex-tank-api/entrypoint.sh
@@ -2,4 +2,7 @@
 set -e
 # To achieve the top most performance you should tune the source server system limits
 ulimit -n 30000
-cd /tank-api-workdir && yandex-tank-api-server --work-dir /tank-api-workdir $@
+cd /tank-api-workdir
+/appenv/bin/yandex-tank-api-server --work-dir /tank-api-workdir
+
+exec "$@"
