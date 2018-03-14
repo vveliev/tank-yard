38bcd09e8211930b647269b2a50b9e02485a62cb
vveliev <vveliev@tucows.com>
2018-03-14 09:56:26 -0400

    api plugin seemed to be the problem , revert back to virtual env. So that pip can be package when need to

--- docker/yandex-tank-api/entrypoint.sh
+++ docker/yandex-tank-api/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 # To achieve the top most performance you should tune the source server system limits
 ulimit -n 30000
 cd /tank-api-workdir
+. /appenv/bin/activate
 /appenv/bin/yandex-tank-api-server --work-dir /tank-api-workdir
 
 exec "$@"
